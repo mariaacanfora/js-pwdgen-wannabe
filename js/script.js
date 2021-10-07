4276cc711d882c0c160d682394a430aaead97c51
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-07 13:13:26 +0200

    Task completed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const nomeUtente = prompt("Buongiorno, come ti chiami?");
+const cognomeUtente = prompt("Qual è il tuo cognome?");
+const colorePreferito = prompt("Infine, qual è il tuo colore preferito?")
+
+alert(`Perfetto!
+La tua password è: ${nomeUtente}${cognomeUtente}${colorePreferito}21 `);
+
+
+console.log(`Nome: ${nomeUtente}
+Cognome: ${cognomeUtente}
+Colore preferito: ${colorePreferito}`);
+
+
